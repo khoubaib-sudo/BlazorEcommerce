299c4bdd305c36c19e61f38293908eab58995dc3
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2022-04-13 00:22:37 +0200

    Number of items in cart counter

---
+++ BlazorEcommerce/Client/Services/CartService/CartService.cs
@@ -0,0 +1,40 @@
+﻿using Blazored.LocalStorage;
+
+namespace BlazorEcommerce.Client.Services.CartService
+{
+    public class CartService : ICartService
+    {
+        private ILocalStorageService _localStorage;
+
+        public CartService( ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        public ILocalStorageService localStorage { get; }
+
+        public event Action OnChange;
+
+        public async Task AddToCart(CartItem cartItem)
+        {
+            var cart = await _localStorage.GetItemAsync<List<CartItem>>("cart");
+            if(cart == null)
+            {
+                cart = new List<CartItem>();
+            }
+            cart.Add(cartItem);
+            await _localStorage.SetItemAsync("cart", cart);
+            OnChange.Invoke();
+        }
+
+        public async Task<List<CartItem>> GetCartItems()
+        {
+            var cart = await _localStorage.GetItemAsync<List<CartItem>>("cart");
+            if (cart == null)
+            {
+                cart = new List<CartItem>();
+            }
+            return cart;
+        }
+    }
+}
