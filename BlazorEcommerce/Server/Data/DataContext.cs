d2b9745ccc89bb16e4f14c4c8649c6d99e5ed202
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2022-04-21 23:38:41 +0200

    hange the CartItem & Add it to the Database

--- BlazorEcommerce/Server/Data/DataContext.cs
+++ BlazorEcommerce/Server/Data/DataContext.cs
@@ -8,8 +8,12 @@
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<CartItem>()
+                .HasKey(ci => new { ci.UserId, ci.ProductId, ci.ProductTypeId });
+
             modelBuilder.Entity<ProductVariant>()
                 .HasKey(p => new { p.ProductId, p.ProductTypeId});
+
             modelBuilder.Entity<ProductType>().HasData(
                 new ProductType { Id = 1, Name = "Default" },
                 new ProductType { Id = 2, Name = "Paperback" },
@@ -477,5 +481,6 @@
         public DbSet<ProductType> ProductTypes { get; set; }
         public DbSet<ProductVariant> ProductVariants { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
     }
 }
