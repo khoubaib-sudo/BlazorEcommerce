f30ee3b8321afbfcb15018ef4cacb4956a82f1f8
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2022-04-30 18:00:17 +0200

    Change the Product & ProductVariant Model for Administrations

---
+++ BlazorEcommerce/Server/Migrations/20220430155658_ProductVisibleDeletFlags.cs
@@ -0,0 +1,451 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorEcommerce.Server.Migrations
+{
+    public partial class ProductVisibleDeletFlags : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "ProductVariants",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Visible",
+                table: "ProductVariants",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Visible",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 1, 2 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 1, 3 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 1, 4 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 2, 2 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 2, 3 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 2, 4 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 3, 2 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 3, 3 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 3, 4 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 4, 2 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 4, 3 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 4, 4 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 5, 2 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 5, 3 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 5, 4 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 6, 2 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 6, 3 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 6, 4 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 7, 5 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 7, 6 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 7, 7 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 8, 5 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 8, 6 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 8, 7 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 9, 5 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 9, 6 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 9, 7 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 10, 5 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 10, 6 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 10, 7 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 11, 8 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 11, 9 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 11, 10 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 12, 8 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 12, 9 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 12, 10 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 13, 8 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 13, 9 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 13, 10 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 14, 8 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 14, 9 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductVariants",
+                keyColumns: new[] { "ProductId", "ProductTypeId" },
+                keyValues: new object[] { 14, 10 },
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "Visible",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "Visible",
+                value: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "ProductVariants");
+
+            migrationBuilder.DropColumn(
+                name: "Visible",
+                table: "ProductVariants");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Visible",
+                table: "Products");
+        }
+    }
+}
